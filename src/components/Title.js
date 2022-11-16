c9379c38cf38e69417130f4e0cdd460ddba07c94
Leonardo Marçal <66973637+Leonnmarshall@users.noreply.github.com>
2022-11-16 20:47:16 -0300

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+  return (
+    <div className='title-container'>
+      <h1>Artigos e dicas úteis</h1>
+      <h3 className='subtitle'>Leia diversos artigos que podem lhe ajudar a ter uma saúde equilibrada e eficaz.</h3>
+    </div>
+  )
+}
+export default Title
